505ed1a39b6022f7b85435578067f0936b9dfc66
Achraf EL MAKAOUI <elmakaouiachraf@gmail.com>
2026-05-05 22:30:30 +0100

    Product & Delete Product

--- src/app/products/products.ts
+++ src/app/products/products.ts
@@ -1,9 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForOf, NgIf } from '@angular/common'
 
 @Component({
   selector: 'app-products',
-  imports: [],
+  imports: [
+    NgForOf, NgIf
+  ],
   templateUrl: './products.html',
   styleUrl: './products.css',
+  standalone: true
 })
-export class Products {}
+export class Products implements OnInit{
+  products : any;
+  constructor() {
+
+  }
+  ngOnInit(){
+    this.products = [
+      { id: 1, name : "Computer", price : 2300, selected : true },
+      { id: 2, name : "Printer", price : 1200, selected : false },
+      { id: 3, name : "Smart Phone", price : 11000, selected : true },
+    ]
+  }
+  handelDelete(products: any){
+    let v = confirm('etes vous sure de vouloir supprimer?');
+    if(v==true){
+      this.products = this.products.filter( (p:any) => p.id != products.id );
+    }
+  }
+}
---
+++ src/app/services/product.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Product {
+  products = [
+    { id: 1, name : "Computer", price : 2300, selected : true },
+    { id: 2, name : "Printer", price : 1200, selected : false },
+    { id: 3, name : "Smart Phone", price : 11000, selected : true },
+  ]
+
+  constructor(){}
+}
