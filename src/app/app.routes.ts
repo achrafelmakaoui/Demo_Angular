b3b6eaa806b9890a3beee2b28901f902719f576b
Achraf EL MAKAOUI <elmakaouiachraf@gmail.com>
2026-05-05 20:50:52 +0100

    Add Routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home'
+import { Products } from './products/products'
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: "Home", component: Home },
+  { path: "Products", component: Products }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  standalone: true,
 })
 export class App {
-  protected readonly title = signal('angularApp');
+  email : string = "achraf@gmail.com";
 }
